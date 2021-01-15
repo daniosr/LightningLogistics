aa67e8770fed5225c51c9046559579373a793eba
BaileyHope <49069406+BaileyHope@users.noreply.github.com>
2021-01-15 21:58:40 +0000

    Update main.dart

--- MobileApp/lightninglogistics/lib/main.dart
+++ MobileApp/lightninglogistics/lib/main.dart
@@ -5,8 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -20,37 +18,18 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
-  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -72,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       decoration: const InputDecoration(
                         icon: Icon(Icons.camera_alt_rounded, size: 30),
                         hintText: 'Scan by clicking camera',
-                        labelText: 'BTWD/Ref*',
+                        labelText: 'BTWD/Reference*',
                       ),
                       onSaved: (String value) {
                       },
@@ -84,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     TextFormField(
                       decoration: const InputDecoration(
-                        labelText: 'SalesOrderRef*',
+                        labelText: 'Sale Order Reference*',
                       ),
                       onSaved: (String value) {
                       },
@@ -96,7 +75,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     TextFormField(
                       decoration: const InputDecoration(
-                        hintText: 'A or K',
                         labelText: 'End*',
                       ),
                       onSaved: (String value) {
@@ -109,7 +87,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     TextFormField(
                       decoration: const InputDecoration(
-                        hintText: 'Numbers only',
                         labelText: 'Weight*',
                       ),
                       onSaved: (String value) {
