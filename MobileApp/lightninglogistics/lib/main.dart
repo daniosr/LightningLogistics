8c6ed4de3f5e97e139d781a50c751cedc24c4823
Daniel Riley <fenergetic@outlook.com>
2021-01-16 02:17:54 +0000

    Cleaned up Mobile app

--- MobileApp/lightninglogistics/lib/main.dart
+++ MobileApp/lightninglogistics/lib/main.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -18,19 +20,39 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
+
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
   void _incrementCounter() {
     setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  @override
   Widget build(BuildContext context) {
+    final node = FocusScope.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Lightning Logistics"),
@@ -41,93 +63,99 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Container(
           padding: EdgeInsets.all(20),
-          child: Column(
+          child: ListView(
             children: <Widget>[
               Form(
-                  child: Column(//change to list view to prevent keyboard error
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.camera_alt_rounded, size: 30),
-                        hintText: 'Scan by clicking camera',
-                        labelText: 'BTWD/Reference*',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                  child: Column(
+                //change to list view to prevent keyboard error
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.camera_alt_rounded, size: 30),
+                      hintText: 'Scan by clicking camera',
+                      labelText: 'BTWD/Ref*',
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Sale Order Reference*',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      labelText: 'Sales Order Reference*',
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'End*',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      labelText: 'End*',
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Weight*',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      labelText: 'Weight*',
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Location*',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      labelText: 'Location*',
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'SubLocation',
-                      ),
-                      onSaved: (String value) {
-                      },
-                      validator: (String value) {
-                        return value.contains('@')
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  TextFormField(
+                    onEditingComplete: () => node.nextFocus(),
+                    decoration: const InputDecoration(
+                      labelText: 'Sub-Location',
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Submit'),
-                    )
-                  ],
-                )
-              )
+                    onSaved: (String value) {},
+                    validator: (String value) {
+                      return value.contains('@')
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Container(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Submit'),
+                  )
+                ],
+              ))
             ],
           ),
         ),
