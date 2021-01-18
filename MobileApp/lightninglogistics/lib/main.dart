911bc9b0146209d91167aaba2d50924c8468f440
BaileyHope <b.hope-2018@hull.ac.uk>
2021-01-18 10:19:12 +0000

    Updated forms

--- MobileApp/lightninglogistics/lib/main.dart
+++ MobileApp/lightninglogistics/lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,15 +20,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -41,11 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
     });
   }
@@ -67,7 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               Form(
                   child: Column(
-                //change to list view to prevent keyboard error
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextFormField(
@@ -76,13 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       icon: Icon(Icons.camera_alt_rounded, size: 30),
                       hintText: 'Scan by clicking camera',
                       labelText: 'BTWD/Ref*',
-                    ),
-                    onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.contains('@')
-                          ? 'Do not use the @ char.'
-                          : null;
-                    },
+                    ),//Add functionality to icon
+                    onSaved: (String value){},
                   ),
                   Container(height: 20),
                   TextFormField(
@@ -90,12 +70,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     decoration: const InputDecoration(
                       labelText: 'Sales Order Reference*',
                     ),
+                    keyboardType: TextInputType.number,
                     onSaved: (String value) {},
-                    validator: (String value) {
+                    validator: (String value) {//Validator for length?
                       return value.contains('@')
                           ? 'Do not use the @ char.'
                           : null;
-                    },
+                    },//....................//
                   ),
                   Container(height: 20),
                   TextFormField(
@@ -104,10 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       labelText: 'End*',
                     ),
                     onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.contains('@')
-                          ? 'Do not use the @ char.'
-                          : null;
+                    validator: (value){
+                      return value.length != 1?  'Name must be only one characters' : null;
                     },
                   ),
                   Container(height: 20),
@@ -116,6 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     decoration: const InputDecoration(
                       labelText: 'Weight*',
                     ),
+                    keyboardType: TextInputType.number,
                     onSaved: (String value) {},
                     validator: (String value) {
                       return value.contains('@')
